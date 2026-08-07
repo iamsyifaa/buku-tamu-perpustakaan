2f006b4f6c7c2194d86d0348420fcb4a52cc129d
iamsyifaa <syifaoktaviani1710@gmail.com>
2026-08-07 13:06:10 +0700

    masih terlempar kembali ke login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        
+        // Tambahkan baris ini agar Laravel 12 menerima proxy dari Vercel
+        $middleware->trustProxies(at: '*');
+
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
